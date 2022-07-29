a5cfc2aa35e5a8e07588a432c4b5482f45a82dd3
DagmawiAbate <dagmawiabate2@gmail.com>
2022-07-29 15:00:27 +0300

    Feature SideDrawer Added

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
+import UsersList from '../components/UsersList'
+
 const Users = () => {
-  return <h2>Users Works!</h2>
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Dagmawi Abate',
+      image: 'https://images.unsplash.com/photo-1658828911599-067ee5eb6a7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80',
+      places: 3
+    }
+  ]
+
+  return <UsersList items={USERS} />
 }
 
 export default Users
