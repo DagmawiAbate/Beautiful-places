18a33bcb0a7cd972d4e05170893919d44fce84f1
DagmawiAbate <dagmawiabate2@gmail.com>
2022-08-13 13:52:24 +0300

    Convert an address into coordinates

---
+++ backend/util/location.js
@@ -0,0 +1,10 @@
+function getCoordsForAddress(address) {
+  const coordinates =  {
+    lat: 40.7484474,
+    lng: -73.9871516
+  }
+
+  return coordinates
+}
+
+module.exports = getCoordsForAddress
