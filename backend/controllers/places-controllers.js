aecf26d3107ecbfee86030ba08dfe94598609f9b
DagmawiAbate <dagmawiabate2@gmail.com>
2022-09-13 13:17:37 +0300

    Uploading imagesfor new places

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -83,8 +83,7 @@ const createPlace = async (req, res, next) => {
     description,
     address,
     location: coordinates,
-    image:
-      'https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80',
+    image: req.file.path,
     creator
   })
 
--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -71,15 +71,18 @@ const PlaceItem = (props) => {
         }
       >
         <p>
-          Do you want to proceed and delete this place? Please note that it can't
-          be undone thereafter.
+          Do you want to proceed and delete this place? Please note that it
+          can't be undone thereafter.
         </p>
       </Modal>
       <li className='place-item'>
         <Card className='place-item__content'>
           {isLoading && <LoadingSpinner asOverlay />}
           <div className='place-item__image'>
-            <img src={props.image} alt={props.title} />
+            <img
+              src={`http://localhost:5000/${props.image}`}
+              alt={props.title}
+            />
           </div>
           <div className='place-item__info'>
             <h2>{props.title}</h2>
