ed69025afaab4cb3c60a4f8a450365e7e6ecf0c8
DagmawiAbate <dagmawiabate2@gmail.com>
2022-09-22 17:05:58 +0300

    Adding auto-login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,69 +1,80 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react'
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
   Switch
-} from 'react-router-dom';
+} from 'react-router-dom'
 
-import Users from './user/pages/Users';
-import NewPlace from './places/pages/NewPlace';
-import UserPlaces from './places/pages/UserPlaces';
-import UpdatePlace from './places/pages/UpdatePlace';
-import Auth from './user/pages/Auth';
-import MainNavigation from './shared/components/Navigation/MainNavigation';
-import { AuthContext } from './shared/context/auth-context';
+import Users from './user/pages/Users'
+import NewPlace from './places/pages/NewPlace'
+import UserPlaces from './places/pages/UserPlaces'
+import UpdatePlace from './places/pages/UpdatePlace'
+import Auth from './user/pages/Auth'
+import MainNavigation from './shared/components/Navigation/MainNavigation'
+import { AuthContext } from './shared/context/auth-context'
 
 const App = () => {
-  const [token, setToken] = useState(false);
-  const [userId, setUserId] = useState(false);
+  const [token, setToken] = useState(false)
+  const [userId, setUserId] = useState(false)
 
   const login = useCallback((uid, token) => {
-    setToken(token);
-    localStorage.setItem('userData', JSON.stringify({userId: uid, token: token }))
-    setUserId(uid);
-  }, []);
+    setToken(token)
+    localStorage.setItem(
+      'userData',
+      JSON.stringify({ userId: uid, token: token })
+    )
+    setUserId(uid)
+  }, [])
 
   const logout = useCallback(() => {
-    setToken(null);
-    setUserId(null);
-  }, []);
+    setToken(null)
+    setUserId(null)
+    localStorage.removeItem('userData')
+  }, [])
 
-  let routes;
+  useEffect(() => {
+    const storeData = JSON.parse(localStorage.getItem('userData'))
+    if(storeData && storeData.token) {
+      login(storeData.userId, storeData.token)
+    }
+  }, [login])
+
+  let routes
 
   if (token) {
     routes = (
       <Switch>
-        <Route path="/" exact>
+        <Route path='/' exact>
           <Users />
         </Route>
-        <Route path="/:userId/places" exact>
+        <Route path='/:userId/places' exact>
           <UserPlaces />
         </Route>
-        <Route path="/places/new" exact>
+        <Route path='/places/new' exact>
           <NewPlace />
         </Route>
-        <Route path="/places/:placeId">
+        <Route path='/places/:placeId'>
           <UpdatePlace />
         </Route>
-        <Redirect to="/" />
+        <Redirect to='/' />
       </Switch>
-    );
+    )
   } else {
     routes = (
       <Switch>
-        <Route path="/" exact>
+        <Route path='/' exact>
           <Users />
         </Route>
-        <Route path="/:userId/places" exact>
+        <Route path='/:userId/places' exact>
           <UserPlaces />
         </Route>
-        <Route path="/auth">
+        <Route path='/auth'>
           <Auth />
         </Route>
-        <Redirect to="/auth" />
+        <Redirect to='/auth' />
       </Switch>
-    );
+    )
   }
 
   return (
@@ -81,7 +92,7 @@ const App = () => {
         <main>{routes}</main>
       </Router>
     </AuthContext.Provider>
-  );
-};
+  )
+}
 
-export default App;
+export default App
