56baead8a0543be551ec35ac26e31b03b3738e11
DagmawiAbate <dagmawiabate2@gmail.com>
2022-09-22 17:58:53 +0300

    Managing the token expiration date

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,13 +18,18 @@ const App = () => {
   const [token, setToken] = useState(false)
   const [userId, setUserId] = useState(false)
 
-  const login = useCallback((uid, token) => {
+  const login = useCallback((uid, token, expirationDate) => {
     setToken(token)
+    setUserId(uid)
+    const tokenExpirationDate = expirationDate || new Date(new Date().getTime())
     localStorage.setItem(
       'userData',
-      JSON.stringify({ userId: uid, token: token })
+      JSON.stringify({
+        userId: uid,
+        token: token,
+        expiration: tokenExpirationDate.toISOString()
+      })
     )
-    setUserId(uid)
   }, [])
 
   const logout = useCallback(() => {
@@ -34,9 +39,17 @@ const App = () => {
   }, [])
 
   useEffect(() => {
-    const storeData = JSON.parse(localStorage.getItem('userData'))
-    if(storeData && storeData.token) {
-      login(storeData.userId, storeData.token)
+    const storedData = JSON.parse(localStorage.getItem('userData'))
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(
+        storedData.userId,
+        storedData.token,
+        new Date(storedData.expiration)
+      )
     }
   }, [login])
 
